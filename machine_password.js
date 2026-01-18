dce15b37593f8da9281463762e1e80b4ed1f3785
lefteristhomas <leuteristhomas@gmail.com>
2026-01-18 18:12:12 +0200

    password generation functionality in JavaScript

---
+++ machine_password.js
@@ -0,0 +1,40 @@
+// code in JavaScript 
+
+const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+-=";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    console.log(allowedChars);
+
+    if(length <= 0){
+        return `(password length must be at least 1)`;
+    }
+    if(allowedChars.length === 0){
+        return `(At least 1 set of characters needs to be selected)`
+    }
+
+    for(let i =0; i<length;i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+    return password
+}
+
+const passwordLength = 20;
+const includeLowercase = false;
+const includeUppercase = true;
+const includeNumbers = false;
+const includeSymbols = false;
+
+const password = generatePassword(passwordLength,includeLowercase,includeUppercase,includeNumbers,includeSymbols)
+
+console.log(`Generated password: ${password}`)
